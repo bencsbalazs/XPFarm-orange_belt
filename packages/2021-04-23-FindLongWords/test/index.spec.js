20645c787ea997ebbfa83dd3b1fa8b53756d6e33
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-03 15:29:53 +0200

    Test-red

--- packages/2021-04-23-FindLongWords/test/index.spec.js
+++ packages/2021-04-23-FindLongWords/test/index.spec.js
@@ -5,4 +5,8 @@ describe("Filter long words", ()=>{
     it("Check the sentence is a string",()=>{
         expect(longWords(2, 1)).to.be.false
     })
+
+    it("Check the input number is not 0",()=>{
+        expect(longWords("This is a long sentence.", 0)).to.be.false
+    })
 })
