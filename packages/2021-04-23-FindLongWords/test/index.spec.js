55714dc7121a232ca7e02b18bc97de267ff03226
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-03 15:42:51 +0200

    UT2-red

--- packages/2021-04-23-FindLongWords/test/index.spec.js
+++ packages/2021-04-23-FindLongWords/test/index.spec.js
@@ -2,11 +2,25 @@ import {expect} from 'chai'
 import {longWords} from '../index.js'
 
 describe("Filter long words", ()=>{
-    it("Check the sentence is a string",()=>{
-        expect(longWords(2, 1)).to.be.false
+    
+    describe("Check input parameters", ()=>{
+        it("Check the sentence is a string",()=>{
+            expect(longWords(2, 1)).to.be.false
+        })
+    
+        it("Check the input number is not 0",()=>{
+            expect(longWords("This is a long sentence.", 0)).to.be.false
+        })
     })
 
-    it("Check the input number is not 0",()=>{
-        expect(longWords("This is a long sentence.", 0)).to.be.false
+    describe("Check the output", ()=>{
+        it("Should return with an array", ()=>{
+            expect(longWords("This is a long sentence",1)).to.be.an("array")
+        })
+
+        it("Should return with the 'sentence' word", ()=>{
+            expect(longWords("This is a long sentence",5)).to.equal(["sentence"])
+        })
     })
+    
 })
