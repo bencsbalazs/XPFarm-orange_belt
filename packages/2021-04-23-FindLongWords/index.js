ea43d3fb5f08b3358a7877da4c2237c17bec5da1
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-03 15:33:35 +0200

    Test-refactor

--- packages/2021-04-23-FindLongWords/index.js
+++ packages/2021-04-23-FindLongWords/index.js
@@ -1,8 +1,10 @@
 export const longWords = (sentence, n) => {
+    let filteredWords = []
     if (typeof(sentence) != "string") {
         return false
     }
     if (n<1) {
         return false
     }
+    return filteredWords
 }
