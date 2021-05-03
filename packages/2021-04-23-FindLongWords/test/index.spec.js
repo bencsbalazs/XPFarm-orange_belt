9b26bc78ea389d394ad7bf12b5647f29f5275281
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-03 15:21:23 +0200

    UT1-red

--- packages/2021-04-23-FindLongWords/test/index.spec.js
+++ packages/2021-04-23-FindLongWords/test/index.spec.js
@@ -1,1 +1,8 @@
 import {expect} from 'chai'
+import {longWords} from '../index.js'
+
+describe("Filter long words", ()=>{
+    it("Check the sentence",()=>{
+        expect(longWords("This is a long sentence.", 1)).to.be.an('array')
+    })
+})
