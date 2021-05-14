1064c8feb35595169a4df330ea1f561a064cb1f8
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-14 17:54:44 +0200

    2021/05/14-2/1-refactor

--- packages/2021-05-14/lib/index.js
+++ packages/2021-05-14/lib/index.js
@@ -1,10 +1,8 @@
 export function limit(array, limit) {
-    if (!Array.isArray(array)) {
+    if (!Array.isArray(array) || array.some(isNaN)) {
         return false;
     }
-    if (array.some(isNaN)){
-        return false;
-    }
+
     if (!Number.isInteger(limit)){
         return "Error";
     }
