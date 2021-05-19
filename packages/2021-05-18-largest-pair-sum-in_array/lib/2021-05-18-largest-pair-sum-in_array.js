8306a24898d6326cfe7281b4c6c4d171b6fd65f7
Balazs Bencs <balazs.bencs@ibm.com>
2021-05-19 13:42:30 +0200

    2021-05-18-2/2-green

--- packages/2021-05-18-largest-pair-sum-in_array/lib/2021-05-18-largest-pair-sum-in_array.js
+++ packages/2021-05-18-largest-pair-sum-in_array/lib/2021-05-18-largest-pair-sum-in_array.js
@@ -2,6 +2,8 @@ export function largestPairSumInArray(array) {
     if (!Array.isArray(array) || array.some(isNaN)) {
         return false;
     }
+    var sorted = sorting(array);
+    return sorted[0] + sorted[1];
     
 }
 
