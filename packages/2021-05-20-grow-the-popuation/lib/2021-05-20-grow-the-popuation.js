8bdd66d3db515a134a841c5d480c88c2b019f162
bencsbalazs <bencsbalazs@gmail.com>
2021-05-23 12:31:51 +0200

    2021-05-20-1/2-refactor

--- packages/2021-05-20-grow-the-popuation/lib/2021-05-20-grow-the-popuation.js
+++ packages/2021-05-20-grow-the-popuation/lib/2021-05-20-grow-the-popuation.js
@@ -1,5 +1,5 @@
 
 export function nbYear(p0, percent, aug, p) {
-    if (p0>p) {return false;}
-    if (typeof(percent) != "number") {return false;}
+    if (p0>p || typeof(percent) != "number") {return false;}
+    
 }
