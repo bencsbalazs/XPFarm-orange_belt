7fbe5e31bb374896c88a9d42123eff6f1eab7053
bencsbalazs <bencsbalazs@gmail.com>
2021-05-23 12:40:47 +0200

    2021-05-20-2/2-green

--- packages/2021-05-20-grow-the-popuation/lib/2021-05-20-grow-the-popuation.js
+++ packages/2021-05-20-grow-the-popuation/lib/2021-05-20-grow-the-popuation.js
@@ -1,5 +1,6 @@
 
 export function nbYear(p0, percent, aug, p) {
     if (p0>p || typeof(percent) != "number" || typeof(aug) != "number") {return false;}
-    
+    for(var y = 0; p0 < p; y++) p0 = p0 * (1 + percent / 100) + aug;
+    return y
 }
