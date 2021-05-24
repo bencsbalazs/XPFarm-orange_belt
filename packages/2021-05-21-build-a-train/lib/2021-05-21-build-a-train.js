6d99e42e674484cb115cb3510705ee3b605b8629
bencsbalazs <bencsbalazs@gmail.com>
2021-05-24 12:41:05 +0200

    2021-05-21-1/2-green

--- packages/2021-05-21-build-a-train/lib/2021-05-21-build-a-train.js
+++ packages/2021-05-21-build-a-train/lib/2021-05-21-build-a-train.js
@@ -1,8 +1,7 @@
 export function buildATrain(train) {
     
     const METALS = {"A": 15, "B": 10, "C": 7, "D": 8, "_": 5};
-    if (!Object.keys(METALS).includes(train.split('')[0]))
-        return false;
-    
+    if (!Object.keys(METALS).includes(train.split('')[0])) return false;
+    if (!train.split("").includes("_")) return false;
     
 }
