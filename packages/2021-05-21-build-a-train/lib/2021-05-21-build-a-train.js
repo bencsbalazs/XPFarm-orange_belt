c9758f917b7b787173de777076e800902ca137b7
bencsbalazs <bencsbalazs@gmail.com>
2021-05-24 12:41:39 +0200

    2021-05-21-1/2-refactor

--- packages/2021-05-21-build-a-train/lib/2021-05-21-build-a-train.js
+++ packages/2021-05-21-build-a-train/lib/2021-05-21-build-a-train.js
@@ -1,7 +1,6 @@
 export function buildATrain(train) {
     
     const METALS = {"A": 15, "B": 10, "C": 7, "D": 8, "_": 5};
-    if (!Object.keys(METALS).includes(train.split('')[0])) return false;
-    if (!train.split("").includes("_")) return false;
+    if (!Object.keys(METALS).includes(train.split('')[0]) || !train.split("").includes("_")) return false;
     
 }
