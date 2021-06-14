a3dccc4eca51ff0a679077c683a6a4d28b95526f
Balazs Bencs <balazs.bencs@ibm.com>
2021-06-14 16:32:51 +0200

    2021-06-14-findTheSquares-2/2-red,green,rf

--- packages/2021-06-14-findTheSquares/__tests__/2021-06-14-findTheSquares.test.js
+++ packages/2021-06-14-findTheSquares/__tests__/2021-06-14-findTheSquares.test.js
@@ -7,7 +7,7 @@ describe('Testing 2021-06-14-findTheSquares', () => {
       expect(findTheSquares(1000002)).equal(false)
     });
     it('It should be false if input is not odd', ()=>{
-      expect(findTheSquares(1)).equal(false);
+      expect(findTheSquares(2)).equal(false);
     });
   });
 
@@ -15,8 +15,8 @@ describe('Testing 2021-06-14-findTheSquares', () => {
     it('Should return false if returning value is not a string', ()=>{
       expect(typeof(findTheSquares(9))).equal("string");
     });
-    it.skip('', ()=>{
-
+    it('Should return 9-4', ()=>{
+      expect(findTheSquares(5)).equal('9-4');
     });
-  })
+  });
 });
--- packages/2021-06-14-findTheSquares/lib/2021-06-14-findTheSquares.js
+++ packages/2021-06-14-findTheSquares/lib/2021-06-14-findTheSquares.js
@@ -1,7 +1,9 @@
 const findTheSquares = (n)=>{
-  if (n % 2 != 0 || n>1000000 || n<0) {
+  if (n % 2 == 0 || n>1000000 || n<0) {
     return false;
   }
+  const i = (n - 1) / 2;
+  return ((i + 1) ** 2)+ '-' + (i ** 2);
 };
 
 module.exports = { findTheSquares };
