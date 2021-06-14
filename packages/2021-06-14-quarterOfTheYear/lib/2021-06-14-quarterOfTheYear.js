6440193420f33944ec60f337b30864fc76a3c181
Balazs Bencs <balazs.bencs@ibm.com>
2021-06-14 15:35:55 +0200

    2021-06-14-2/1-green

--- packages/2021-06-14-quarterOfTheYear/lib/2021-06-14-quarterOfTheYear.js
+++ packages/2021-06-14-quarterOfTheYear/lib/2021-06-14-quarterOfTheYear.js
@@ -2,6 +2,12 @@ const quarterOfTheYear = (param)=>{
   if (typeof(param) != "number" || param > 12 || param < 1) {
     return false;
   }
+  switch(true){
+    case (param<4): return 1;
+    case (param<7): return 2;
+    case (param<10): return 3;
+    case (param<13): return 4;
+  }
 };
 
 module.exports = { quarterOfTheYear };
