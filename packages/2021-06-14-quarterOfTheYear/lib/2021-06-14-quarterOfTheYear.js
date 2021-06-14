11d02edd07a305fa97826955d31c7c633976ce70
Balazs Bencs <balazs.bencs@ibm.com>
2021-06-14 15:44:53 +0200

    2021-06-14-2/2-refactor

--- packages/2021-06-14-quarterOfTheYear/lib/2021-06-14-quarterOfTheYear.js
+++ packages/2021-06-14-quarterOfTheYear/lib/2021-06-14-quarterOfTheYear.js
@@ -1,17 +1,6 @@
 const quarterOfTheYear = (param) => {
-  if (typeof param != "number" || param > 12 || param < 1) {
-    return false;
-  }
-  switch (true) {
-    case param < 4:
-      return 1;
-    case param < 7:
-      return 2;
-    case param < 10:
-      return 3;
-    case param < 13:
-      return 4;
-  }
+  if (typeof param != "number" || param > 12 || param < 1) {return false;}
+  return Math.ceil(param/3);
 };
 
 module.exports = { quarterOfTheYear };
