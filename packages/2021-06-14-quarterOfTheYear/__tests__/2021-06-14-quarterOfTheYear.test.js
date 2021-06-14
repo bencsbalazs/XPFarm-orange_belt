e167af7c448291a55205e51501e32112a46510ee
Balazs Bencs <balazs.bencs@ibm.com>
2021-06-14 15:38:24 +0200

    2021-06-14-2/2-red

--- packages/2021-06-14-quarterOfTheYear/__tests__/2021-06-14-quarterOfTheYear.test.js
+++ packages/2021-06-14-quarterOfTheYear/__tests__/2021-06-14-quarterOfTheYear.test.js
@@ -1,19 +1,22 @@
-const { expect } = require('chai');
-const { quarterOfTheYear } = require('../lib/2021-06-14-quarterOfTheYear.js');
+const { expect } = require("chai");
+const { quarterOfTheYear } = require("../lib/2021-06-14-quarterOfTheYear.js");
 
-describe('Testing 2021-06-14-quarterOfTheYear', () => {
-  describe('User story 1', ()=>{
-    it('Should be false if the parameter is not integer', ()=>{
+describe("Testing 2021-06-14-quarterOfTheYear", () => {
+  describe("User story 1", () => {
+    it("Should be false if the parameter is not integer", () => {
       expect(quarterOfTheYear("not number")).equal(false);
     });
-    it('Should be false if the parameter is greater than 12', ()=>{
+    it("Should be false if the parameter is greater than 12", () => {
       expect(quarterOfTheYear(13)).equal(false);
     });
   });
 
-  describe('User story 2', ()=>{
-    it("Accepted if the result is between 1-4", ()=>{
-      expect(quarterOfTheYear(2)).lessThan(4)
-    })
+  describe("User story 2", () => {
+    it("Accepted if the result is between 1-4", () => {
+      expect(quarterOfTheYear(2)).lessThan(4);
+    });
+    it("Accepted if the result is 2", () => {
+      expect(quarterOfTheYear(2)).equals(2);
+    });
   });
 });
