bd463c8de92653bf03d706d87e8775af6a9660d8
Balazs Bencs <balazs.bencs@ibm.com>
2021-06-14 15:10:52 +0200

    2021-06-14-1/1-red

--- packages/2021-06-14-quarterOfTheYear/__tests__/2021-06-14-quarterOfTheYear.test.js
+++ packages/2021-06-14-quarterOfTheYear/__tests__/2021-06-14-quarterOfTheYear.test.js
@@ -3,12 +3,12 @@ const { quarterOfTheYear } = require('../lib/2021-06-14-quarterOfTheYear.js');
 
 describe('Testing 2021-06-14-quarterOfTheYear', () => {
   describe('User story 1', ()=>{
-    it('needs tests', ()=>{
-      expect(quarterOfTheYear()).equal(true);
+    it('Should be false if the parameter is not integer', ()=>{
+      expect(quarterOfTheYear("not number")).equal(false);
     });
   });
 
   describe('User story 2', ()=>{
 
-  })
+  });
 });
